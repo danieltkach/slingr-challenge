c8e7f85c58f8936b6ed9d76bb270ac5ff5fd8171
Daniel Tkach <danieltkach@gmail.com>
2024-06-14 16:08:31 -0300

    theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-function App() {
-  return (
-    <>
-          
-    </>
-  )
-}
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 
-export default App
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4D81B7'
+    },
+  },
+});
+
+export const App: React.FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <StyledThemeProvider theme={theme}>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <AppBar component="nav">
+            <Toolbar>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+              >
+                SHOPPING LIST
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </StyledThemeProvider>
+    </ThemeProvider>
+  );
+};
