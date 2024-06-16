6273c074f9145656334bf04645cae1deef63b68c
Daniel Tkach <danieltkach@gmail.com>
2024-06-16 15:43:57 -0300

    fix/ add item on empty

--- client/src/components/EmptyListPanel.tsx
+++ client/src/components/EmptyListPanel.tsx
@@ -3,11 +3,15 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Card } from './Card';
 import { Button } from './Button';
+import { routeUrls } from '../config/routes';
+import { useNavigate } from 'react-router-dom';
 
 const title = "Your shopping list is empty :(";
 const buttonText = "Add your first item";
 
 export const EmptyListPanel: React.FC = () => {
+  const navigateTo = useNavigate();
+  
   return (
     <Box
       sx={{
@@ -22,7 +26,11 @@ export const EmptyListPanel: React.FC = () => {
           {title}
         </Typography>
         <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-          <Button text={buttonText} variant={"contained"} />
+          <Button 
+            text={buttonText} 
+            variant={"contained"} 
+            onClick={()=>navigateTo(routeUrls.editItem)}
+          />
         </Box>
       </Card>
     </Box>
